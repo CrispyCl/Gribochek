10a79f04e1e4f0647d021681b547a0d54d5607c6
Gogasmerter <chebunin19@gmail.com>
2023-03-30 21:25:34 +0400

    last commit

--- data/test.py
+++ data/test.py
@@ -7,7 +7,7 @@ g = requests.get('http://localhost:8080/api/audiences_week',
                  json={'date': '2023-04-23'}).json()
 pprint(g)
 
-# Расписание конкретной аудитории
+# # Расписание конкретной аудитории
 # g = requests.get('http://localhost:8080/api/audiences_week/1',
 #                  json={'date': '2023-04-23'}).json()
 # pprint(g)
